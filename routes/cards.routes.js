79be1636b1f83276bc1ee90a74e83396a43382c9
YoyoBLemail <yoelbl2@gmail.com>
2024-01-18 14:05:13 +0200

    card.put = updateCardById endpoint

--- routes/cards.routes.js
+++ routes/cards.routes.js
@@ -1,4 +1,6 @@
+const { getCardById } = require("../lib/cards.lib");
 const { authByRole } = require("../middleware/authorize.mw");
+const { getToken } = require("../middleware/getToken.mw");
 const { validateMongoId } = require("../middleware/validateMongoId.mw");
 const { validateCard, Card } = require("../models/cards.model");
 
@@ -19,6 +21,7 @@ router.post("/", authByRole("business"), async (req, res) => {
    if (error)
       return res.status(400).send("Joi error:" + error.details[0].message);
    //process
+   console.log(req.user._id);
    const newCard = new Card({
       ...req.body,
       user_id: req.user._id,
@@ -32,14 +35,27 @@ router.post("/", authByRole("business"), async (req, res) => {
    }
 });
 
-//get card by id
-router.get("/:id", validateMongoId, authByRole("user"), async (req, res) => {
-   const card = await Card.findById(req.params.id).catch((err) =>
-      res.status(500).send(err)
-   );
-   if (!card) return res.status(400).send("Card id doesn't exist.");
-
+//GET CARD BY ID
+router.get("/:id", validateMongoId, getToken, async (req, res) => {
+   const card = await getCardById(req, res);
+   if (!card) return;
    res.json(card);
+});
+
+//UPDATE CARD BY ID
+router.put("/:id", validateMongoId, getToken, async (req, res) => {
+   //validate user
+   const { error } = validateCard(req.body);
+   if (error) return res.status(400).send(error.details[0].message);
+
+   //validate system
+   const card = await getCardById(req, res);
+   if (!card) return;
+
+   const updateCard = await Card.findByIdAndUpdate(card._id, req.body, {
+      new: true,
+   });
+   res.json(updateCard);
 });
 
 module.exports = router;
